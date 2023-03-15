ce8532e8f44b2292d6b6626d0ab367a597438492
Diego Melo <diegoo-melo@uni9.edu.br>
2023-03-14 23:28:37 -0300

    consumida lista e imagens api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React, {useEffect, useState} from "react";
-import TMdb from "./TMdb";
+import './App.css';
+import Tmdb from "./Tmdb";
+import MovieRow from "./componentes/MovieRow";
+import FeaturedMovie from "./componentes/FeaturedMovie";
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] =useState(null);
 
   useEffect(()=>{
     const loadAll = async () =>{
       //Pegando a lista Total
-      let list = await TMdb.getHomeList();
+      let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      // Pegando o Featured
+      let originals = list.filter(i=>i.slug ==='originals');
+      let randomChosen = Math.floor(Math.random() *(originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);      
     }
 
     loadAll();
@@ -17,11 +28,20 @@ export default () => {
 
   return(
     <div className="page">
-      Header
+
+      {featuredData &&
+        <FeaturedMovie item={featuredData} />
+      }
+
+      <section className="listas">
+        {movieList.map((item, key)=>(
+          <MovieRow key={key} title={item.title} items={item.items}/>
+        ))}
+      </section>
+      /*Header
       Destaque
       As Listas
-      rodape
-      PAREI AS 45MIN
+      rodape*/
     </div>
   );
 }
